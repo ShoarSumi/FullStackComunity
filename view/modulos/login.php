f079ae9b4dc82170864d1fdf87960b419db4737c
Shoar Sumi <shoar.sumi.8@gmail.com>
2020-05-01 12:19:55 -0400

    configuracion del login responsivo para todos los tamaños

--- view/modulos/login.php
+++ view/modulos/login.php
@@ -40,10 +40,11 @@
 
                                     <div class="card-footer">
                                         <div class="d-flex justify-content-center links">
-                                            Don't have an account?<a href="#">Sign Up</a>
+                                        ¿No tienes una cuenta?
+                                            <a href="#">Regístrate</a>
                                         </div>
                                         <div class="d-flex justify-content-center">
-                                            <a href="#">Forgot your password?</a>
+                                            <a href="#">¿Olvidaste tu contraseña?</a>
                                         </div>
                                     </div>
 
